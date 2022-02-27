21cb5bcb1f6db631005e4ebd9475a99fbb01cbd9
stan_bk <2568143457@qq.com>
2022-02-27 16:40:51 +0800

    feat

---
+++ Array/some.js
@@ -0,0 +1,15 @@
+// Array.prototype.some ES5
+// 对数组的每一项进行判断，遇到符合要求则返回true，否则返回false
+Array.prototype.Mysome = function(fnc) {
+  if (typeof fnc !== 'function') {
+    throw new Error(fnc + ' is not a function')
+  }
+  if (Object.prototype.toString.call(this) !== '[object Array]') {
+    return false
+  }
+  for (var i = 0; i < this.length; i++) {
+    if (fnc(this[i]))
+      return true
+  }
+  return false
+}
