628a9df1e5d97eff7a95a7c5532795304a9ba9bc
stan_bk <2568143457@qq.com>
2022-03-01 14:09:47 +0800

    fix: 补充提供给回调函数的参数

--- Array/every.js
+++ Array/every.js
@@ -1,14 +1,15 @@
 // Array.prototype.every ES5
 // 对数组的每一项进行判断，遇到不符合要求则返回false，否则返回true
-Array.prototype.Myevery = function(fnc) {
+Array.prototype.Myevery = function(fnc, thisArg) {
   if (typeof fnc !== 'function') {
     throw new Error(fnc + ' is not a function')
   }
   if (Object.prototype.toString.call(this) !== '[object Array]') {
     return true
   }
+  var obj = Object(this)
   for (var i = 0; i < this.length; i++) {
-    if (!fnc(this[i]))
+    if (!fnc.call(thisArg, obj[i]), i, obj)
       return false
   }
   return true
--- Array/some.js
+++ Array/some.js
@@ -1,14 +1,15 @@
 // Array.prototype.some ES5
 // 对数组的每一项进行判断，遇到符合要求则返回true，否则返回false
-Array.prototype.Mysome = function(fnc) {
+Array.prototype.Mysome = function(fnc, thisArg) {
   if (typeof fnc !== 'function') {
     throw new Error(fnc + ' is not a function')
   }
   if (Object.prototype.toString.call(this) !== '[object Array]') {
     return false
   }
+  var obj = Object(this)
   for (var i = 0; i < this.length; i++) {
-    if (fnc(this[i]))
+    if (fnc.call(thisArg, obj[i], i, obj))
       return true
   }
   return false
