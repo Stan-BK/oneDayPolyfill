c78881407cb5b671e56a499e0e7717c91427591c
stan-BK <2568143457@qq.com>
2022-07-19 10:29:25 +0800

    feat

--- VueUse/src/views/useDocumentVisibility.vue
+++ VueUse/src/views/useDocumentVisibility.vue
@@ -1,8 +1,18 @@
 <script setup lang="ts">
+import { watch, ref } from 'vue'
+import { useDocumentVisibility } from '@/composables';
 
+const visibility = useDocumentVisibility()
+const count = ref(0)
+watch(visibility, () => {
+  count.value++
+})
 </script>
 <template>
-  <div class="documentvisibility"></div>
+  <div class="documentvisibility">
+    <h1>Current page visibility state is: {{ visibility }}</h1>
+    <h2>page had hidden <span style="color: lightgreen">{{ count / 2 }}</span> times</h2>
+  </div>
 </template>
 <style scoped>
 .documentvisibility {
